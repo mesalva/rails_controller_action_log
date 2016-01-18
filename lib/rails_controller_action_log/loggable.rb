af430099da3dee3c75b51270e1dab1580ef1ff9a
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2016-01-18 16:27:55 -0200

    Add datetime entry

--- lib/rails_controller_action_log/loggable.rb
+++ lib/rails_controller_action_log/loggable.rb
@@ -7,6 +7,7 @@ module Loggable extend ActiveSupport::Concern
 	def log_action
 		ControllerLog.create!({controller_name: self.controller_name, 
 			resource_id: params[:id], 
+			updated_at: Time.now,
 			content: request.env.inspect})
 	end
 
